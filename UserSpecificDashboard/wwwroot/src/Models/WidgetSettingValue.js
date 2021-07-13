e82ff72dac1ef3aa9b375f66bfd5229ec5428de5
jdanylko <jdanylko@gmail.com>
2021-07-13 13:11:44 -0400

    Update appsettings.json

--- UserSpecificDashboard/wwwroot/src/Models/WidgetSettingValue.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WidgetSettingValue = void 0;
-class WidgetSettingValue {
-}
-exports.WidgetSettingValue = WidgetSettingValue;
-//# sourceMappingURL=WidgetSettingValue.js.map
